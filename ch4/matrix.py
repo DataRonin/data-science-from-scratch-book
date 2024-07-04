606d16a0f8aa44015c7780dc4a3d7b8bee88fb47
MLRonin <dataronin1227@gmail.com>
2024-07-04 18:37:40 +0800

    Update matrix.py

--- ch4/matrix.py
+++ ch4/matrix.py
@@ -1,20 +1,30 @@
+# matrix, chapter 4
+
 matrix = [[1,2,3],
-          [4,5,6],
-          [7,8,9]]
+     [4,5,6],
+     [7,8,9]]
 
-def get_columns_and_rows(matrix: list) -> tuple:
-    rows = len(matrix)
-    columns = len(matrix[0])
-    return (columns, rows)
-assert get_columns_and_rows(matrix) == (3,3) # true
+def shape(m: list) -> tuple:
+    # m = matrix
+    rows = len(m)
+    columns = len(m[0]) if m else 0
+    return (rows, columns)
+assert shape(matrix) == (3,3)
+# matrix 3x3
 
-def get_row(matrix: list, row: int) -> list:
-    return matrix[row]
-assert get_row(matrix, 1) == [4,5,6]
+def get_row(m: list, i: int) -> list:
+    # m = matrix
+    # i = index
+    # list = vector
+    return m[i-1]
+assert get_row(matrix,3) == [7,8,9]
 
-def get_column(matrix: list, row: int) -> list:
-    return [m[row] for m in matrix]
-assert get_column(matrix, 1) == [2,5,8]
+def get_column(m: list, i: int) -> list:
+    # m = matrix
+    # i = index
+    # list = vector
+    return [el[i-1] for el in m]
+assert get_column(matrix,3) == [3,6,9] 
 
 friend_matrix = [[0,1,1,0,0], # user-1
                  [1,0,0,1,0], # user-2
